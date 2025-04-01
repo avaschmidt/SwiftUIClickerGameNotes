1cb266e70eacad249fb53a3281aa5af2c01916b9
avaschmidt <ava.schmidt25@d155.org>
2025-04-01 07:49:37 -0500

    Initial Commit

---
+++ SwiftUIClickerGameNotes/SwiftUIClickerGameNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIClickerGameNotesApp.swift
+//  SwiftUIClickerGameNotes
+//
+//  Created by AVA SCHMIDT on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIClickerGameNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
